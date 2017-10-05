0750ff35380096d96c0af384f500fcc7fef64a8f
Tyler Bui-Palsulich <tpalsulich@google.com>
2017-10-05 16:25:28 -0700

    server: clean up hash checks and unexport version

--- tika/server.go
+++ tika/server.go
@@ -110,32 +110,31 @@ func (s Server) waitForStart(ctx context.Context) error {
 	}
 }
 
-func validateFileMD5(path, wantH string) (bool, string) {
+func md5Hash(path string) (string, error) {
 	f, err := os.Open(path)
 	if err != nil {
-		return false, ""
+		return "", err
 	}
 	defer f.Close()
 
 	h := md5.New()
 	if _, err := io.Copy(h, f); err != nil {
-		return false, ""
+		return "", err
 	}
-	md5 := fmt.Sprintf("%x", h.Sum(nil))
-	return md5 == wantH, md5
+	return fmt.Sprintf("%x", h.Sum(nil)), nil
 }
 
-// A Version represents a Tika Server version.
-type Version string
+// A version represents a Tika Server version.
+type version string
 
 // Supported versions of Tika Server.
 const (
-	Version114 Version = "1.14"
-	Version115 Version = "1.15"
-	Version116 Version = "1.16"
+	Version114 version = "1.14"
+	Version115 version = "1.15"
+	Version116 version = "1.16"
 )
 
-var md5s = map[Version]string{
+var md5s = map[version]string{
 	Version114: "39055fc71358d774b9da066f80b1141c",
 	Version115: "80bd3f00f05326d5190466de27d593dd",
 	Version116: "6a549ce6ef6e186e019766059fd82fb2",
@@ -147,15 +146,17 @@ var md5s = map[Version]string{
 // It is the caller's responsibility to remove the file when no longer needed.
 // If the file already exists and has the correct MD5, DownloadServer will
 // do nothing.
-func DownloadServer(ctx context.Context, version Version, path string) error {
-	wantH := md5s[version]
-	if wantH == "" {
-		return fmt.Errorf("unsupported Tika version: %s", version)
+func DownloadServer(ctx context.Context, v version, path string) error {
+	hash := md5s[v]
+	if hash == "" {
+		return fmt.Errorf("unsupported Tika version: %s", v)
 	}
 
 	if _, err := os.Stat(path); err == nil {
-		if ok, _ := validateFileMD5(path, wantH); ok {
-			return nil
+		if got, err := md5Hash(path); err == nil {
+			if got == hash {
+				return nil
+			}
 		}
 	}
 	out, err := os.Create(path)
@@ -164,7 +165,7 @@ func DownloadServer(ctx context.Context, version Version, path string) error {
 	}
 	defer out.Close()
 
-	url := fmt.Sprintf("http://search.maven.org/remotecontent?filepath=org/apache/tika/tika-server/%s/tika-server-%s.jar", version, version)
+	url := fmt.Sprintf("http://search.maven.org/remotecontent?filepath=org/apache/tika/tika-server/%s/tika-server-%s.jar", v, v)
 	resp, err := ctxhttp.Get(ctx, nil, url)
 	if err != nil {
 		return fmt.Errorf("unable to download %q: %v", url, err)
@@ -175,11 +176,16 @@ func DownloadServer(ctx context.Context, version Version, path string) error {
 		return fmt.Errorf("error saving download: %v", err)
 	}
 
-	if ok, md5 := validateFileMD5(path, wantH); !ok {
+	h, err := md5Hash(path)
+
+	if err != nil {
+		return err
+	}
+	if h != hash {
 		if err := os.Remove(path); err != nil {
-			return fmt.Errorf("invalid md5: %s: error removing %s: %v", md5, path, err)
+			return fmt.Errorf("invalid md5: %s: error removing %s: %v", h, path, err)
 		}
-		return fmt.Errorf("invalid md5: %s", md5)
+		return fmt.Errorf("invalid md5: %s", h)
 	}
 	return nil
 }
